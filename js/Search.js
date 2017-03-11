7faa5f4b5b2f7a7cada7c8b41722df0ff30568ae
“kasper341” <kasper_341@hotmail.com>
2017-03-11 14:39:57 -0800

    add serach page component

---
+++ js/Search.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Search = React.createClass({
+  render () {
+    return (
+      <div>
+        <h1>
+         Seach !!
+        </h1>
+      </div>
+   )
+  }
+})
+
+export default Search
