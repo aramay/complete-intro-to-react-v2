03354c5f9e49fdb754d432a0c09c8e28a847962b
“kasper341” <kasper_341@hotmail.com>
2017-05-02 09:17:32 -0700

    add filter to search for shows

--- js/Search.js
+++ js/Search.js
@@ -3,15 +3,35 @@ import ShowCard from './ShowCard'
 import preload from '../public/data.json'
 
 const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: ''
+    }
+  },
+  handleSearchTermChange (event) {
+    this.setState({
+      searchTerm: event.target.value
+    })
+  },
   render () {
     return (
       <div className='search'>
-        {preload.shows.map((show) => {
-          return (
-            // <ShowCard key={show.imdbID} show={show} />
-            <ShowCard key={show.imdbID} {...show} />
-        )
+        <header>
+          <h1>svideo</h1>
+          <input onChange={this.handleSearchTermChange} value={this.state.searchTerm} type='text' placeholder='Search' />
+        </header>
+        <div>
+          {preload.shows
+            .filter( (show) => {
+              return `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
+            })
+            .map((show) => {
+            return (
+              // <ShowCard key={show.imdbID} show={show} />
+              <ShowCard key={show.imdbID} {...show} />
+          )
         })}
+        </div>
       </div>
    )
   }
