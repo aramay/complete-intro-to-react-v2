8f8c742d12d19a4427a50c365b5cbb1de7b9d466
“kasper341” <kasper_341@hotmail.com>
2017-05-01 21:02:32 -0700

    preload shows data

--- js/Search.js
+++ js/Search.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import preload from '../public/data.json'
 
 const Search = React.createClass({
   render () {
     return (
-      <div>
+      <div className='search'>
         <h1>
          Seach !!
+         {preload.shows.map( (show) => {
+           return (
+             <h3>{show.title}</h3>
+           )
+         })}
         </h1>
       </div>
    )
