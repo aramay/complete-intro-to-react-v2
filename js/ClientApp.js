ed5fd5a8cf37d5eec6ac72d762bb4b2290a5663d
“kasper341” <kasper_341@hotmail.com>
2017-05-01 20:05:33 -0700

    Fix ReactRouter

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {render} from 'react-dom'
+import { render } from 'react-dom'
 import { BrowserRouter, Match } from 'react-router'
 import Landing from './Landing'
 import Search from './Search'
@@ -7,16 +7,17 @@ import '../public/normalize.css'
 import '../public/style.css'
 
 const App = React.createClass({
-  render () {
+  render: function () {
     return (
-      <BrowserRouter>
-        <div className='app'>
-          <Match exactly pattern='/' component={Landing} />
-          <Match pattern='/search' component={Search} />
-        </div>
-      </BrowserRouter>
-    )
+        <BrowserRouter>
+            <div className='app'>
+                <Match exactly pattern='/' component={Landing}></Match>
+                <Match pattern='/search' component={Search}></Match>
+            </div>
+        </BrowserRouter>
+      )
   }
 })
 
 render(<App />, document.getElementById('app'))
+
