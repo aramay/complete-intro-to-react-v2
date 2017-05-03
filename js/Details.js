04cd1657339cf068b60bddb8584cf7ede73f7d3d
“kasper341” <kasper_341@hotmail.com>
2017-05-03 13:25:32 -0700

    update 'Details' component to display show

--- js/Details.js
+++ js/Details.js
@@ -1,14 +1,50 @@
 import React from 'react'
+const { shape, string } = React.PropTypes
 
 const Details = React.createClass({
+   propsTypes: {
+      show: shape({
+         title: string,
+         year: string,
+         poster: string,
+         description: string,
+         trailer: string
+      })
+
+   },
+
 
    render () {
 
+      const { poster, title, year, description, trailer } = this.props.show
       console.log(this.props)
+      
       return (
-         <h1>test</h1>
+
+      <div className='details'>
+         <header>
+            <h1>svideo</h1>
+         </header>
+         <section>
+            <h1>{title}</h1>
+            <h2>({year})</h2>
+            <img src={`/public/img/posters/${poster}`} />
+            <p>{description}</p>
+         </section>
+         <div>
+            <iframe src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`} frameBorder='0' allowFullScreen />
+         </div>
+      </div>
+            
       )
    }
 })
 
+// stateless compponent can take props -> query params property
+
+// const Details = (props) => {
+//   return <h1>stateless component </h1>
+// }
+
 export default Details
+
