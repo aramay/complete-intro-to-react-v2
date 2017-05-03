dee181cc8f8b5f87464a23a9dbbce91c9df324ee
“kasper341” <kasper_341@hotmail.com>
2017-05-03 09:29:51 -0700

    add tst for 'ShowCard'

--- js/Search.spec.js
+++ js/Search.spec.js
@@ -1,9 +1,30 @@
 import React from 'react'
 import Search from './Search'
-import renderer from 'react-test-renderer'
+
+import { shallow } from 'enzyme'
+import { shallowToJson } from 'enzyme-to-json'
+import ShowCard from './ShowCard'
+import preload from '../public/data.json'
+
+
 
 test('Search should search title', () => {
-  const component = renderer.create(<Search />)
-  const tree = component.toJSON()
+  const component = shallow(<Search />)
+  const tree = shallowToJson(component)
   expect(tree).toMatchSnapshot()
+})
+
+
+// add new test at the bottom
+test('Search should render correct amount of shows', () => {
+  const component = shallow(<Search />)
+  expect(preload.shows.length).toEqual(component.find(ShowCard).length)
+})
+
+test('Search should render correct amount of shows based on search', () => {
+  const searchWord = 'house'
+  const component = shallow(<Search />)
+  component.find('input').simulate('change',{target:{value: searchWord}})
+  const showCount = preload.shows.filter((show) => `${show.title.toUpperCase()} ${show.description.toUpperCase()}`.includes(searchWord.toUpperCase())).length
+  expect(showCount).toEqual(component.find(ShowCard).length)
 })
