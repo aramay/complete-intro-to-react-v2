b2f593a9a8892d579e1db7bdc4b06537cab5b15a
“kasper341” <kasper_341@hotmail.com>
2017-05-03 09:46:19 -0700

    add 'Details' component

---
+++ js/Details.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Details = React.createClass({
+
+   render () {
+
+      console.log(this.props)
+      return (
+         <h1>test</h1>
+      )
+   }
+})
+
+export default Details
