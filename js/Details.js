840e0f56394b70efd48a24e1402ad8fb2d71e2f4
“kasper341” <kasper_341@hotmail.com>
2017-05-03 20:02:43 -0700

    add Show ratings

--- js/Details.js
+++ js/Details.js
@@ -1,32 +1,60 @@
 import React from 'react'
+import axios from 'axios'
 import Header from './Header'
 const { shape, string } = React.PropTypes
 
 const Details = React.createClass({
-   propsTypes: {
-      show: shape({
-         title: string,
-         year: string,
-         poster: string,
-         description: string,
-         trailer: string
+  propsTypes: {
+     show: shape({
+        title: string,
+        year: string,
+        poster: string,
+        description: string,
+        trailer: string
       })
 
    },
 
+  getInitialState () {
+     return {
+        omdbData: {}
+     }
+   },
+  componentDidMount () {
+      axios.get(`http://www.omdbapi.com/?i=${this.props.show.imdbID}`)
+      .then((res) => {
+         console.log("omdb data ", res.data)
+         this.setState({omdbData: res.data})
+      })
+      .catch((error) => {
+         console.log("Axios error ", error)
+      })
+   },
 
-   render () {
+  render () {
+     const { poster, title, year, description, trailer } = this.props.show
 
-      const { poster, title, year, description, trailer } = this.props.show
-      console.log(this.props)
-      
-      return (
+     console.log("Details component props ", this.props)
+
+     let ratings
+
+     if (this.state.omdbData.imdbRatings) {
+        ratings = <h3>{this.state.omdbData.imdbRating}</h3>
+     }
+     else {
+        ratings = <h3>No Ratings found</h3>
+     }
+
+     console.log("ratings ",ratings)
+
+     return (
 
       <div className='details'>
-         <Header></Header>
+         <Header />
          <section>
             <h1>{title}</h1>
             <h2>({year})</h2>
+            {ratings}
             <img src={`/public/img/posters/${poster}`} />
             <p>{description}</p>
          </section>
@@ -34,7 +62,7 @@ const Details = React.createClass({
             <iframe src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`} frameBorder='0' allowFullScreen />
          </div>
       </div>
-            
+
       )
    }
 })
