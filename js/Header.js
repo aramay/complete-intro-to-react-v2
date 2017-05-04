d3960a3ed3a1de2c389ec1cfbf2720f9249f7025
“kasper341” <kasper_341@hotmail.com>
2017-05-03 17:51:39 -0700

    Added 'Header' component

---
+++ js/Header.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+class Header extends React.Component {
+  render () {
+   console.log(this.props)
+
+     let utilSpace
+     //showSearch is true by default when passed in as props
+     if(this.props.showSearch){
+        utilSpace = <input onChange={this.props.handleSearchTermChange} value={this.props.searchTerm} type='text' placeholder='Search' />
+     }
+     else {
+        utilSpace = (
+           <h2>
+            <Link to='/search'>Back</Link>
+           </h2>
+        )
+     }
+    return (
+         <header>
+            <h1>
+               <Link to='/'>
+                  svideo
+               </Link>
+            </h1>
+         {utilSpace}
+         </header>
+      )
+  }
+}
+const {func, bool, string } = React.PropTypes
+
+Header.propType = {
+  handleSearchTermChange: func,
+  showSearch: bool,
+  searchTerm: string
+}
+
+export default Header
