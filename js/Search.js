d1a9ca8e7069a73c7dddf4338f6a4737e45f5ee1
“kasper341” <kasper_341@hotmail.com>
2017-05-03 17:49:13 -0700

    Update 'Search' component to use conditional rendering

--- js/Search.js
+++ js/Search.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ShowCard from './ShowCard'
+import Header from './Header'
 const { arrayOf, shape, string } = React.PropTypes
 // import preload from '../public/data.json'
 
@@ -23,10 +24,11 @@ const Search = React.createClass({
   render () {
     return (
       <div className='search'>
-        <header>
-          <h1>svideo</h1>
-          <input onChange={this.handleSearchTermChange} value={this.state.searchTerm} type='text' placeholder='Search' />
-        </header>
+        <Header
+          showSearch
+          searchTerm={this.state.searchTerm}
+          handleSearchTermChange={this.handleSearchTermChange}
+        />
         <div>
           {this.props.shows
             .filter( (show) => {
