ddafbf6d4ae90d55588e87bc909d0688c3928918
“kasper341” <kasper_341@hotmail.com>
2017-05-04 09:20:35 -0700

    pass state from redux as props to Landing

--- js/Landing.js
+++ js/Landing.js
@@ -1,16 +1,29 @@
 import React from 'react'
 import {Link} from 'react-router'
 
+import { connect } from 'react-redux'
+
+const { string } = React.PropTypes
+
 const Landing = React.createClass({
+  propTypes: {
+    searchTerm: string
+  },
   render () {
     return (
       <div className='landing'>
         <h1>svideo</h1>
-        <input type='text' placeholder='Search' />
+        <input value={this.props.searchTerm} type='text' placeholder='Search' />
         <Link to='/search'>or Browse All</Link>
       </div>
       )
   }
 })
 
-export default Landing
+const mapStateToProps = (state) => {
+  return {
+    searchTerm: state.searchTerm
+  }
+}
+
+export default connect(mapStateToProps)(Landing)
