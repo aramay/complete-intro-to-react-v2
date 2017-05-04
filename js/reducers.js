dbaa5579ceb6abae04f19f78782fe5cfdc55e770
“kasper341” <kasper_341@hotmail.com>
2017-05-03 20:43:17 -0700

    rename file to reducers

---
+++ js/reducers.js
@@ -0,0 +1,14 @@
+const DEFAULT_STATE = {
+  searchTerm: ''
+}
+
+const rootReducer = (state = DEFAULT_STATE, action) => {
+  switch (action.type) {
+   
+
+    default:
+      return state
+  }
+}
+
+export default rootReducer
